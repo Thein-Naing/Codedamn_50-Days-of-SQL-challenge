f4d329874243878173281538b4aa05bb396301e3
Thein Naing <theinnaing0908@gmail.com>
2023-10-28 10:12:40 +0800

    Update PatientsWithNonAllergies.sql

--- PatientsWithNonAllergies.sql
+++ PatientsWithNonAllergies.sql
@@ -1,4 +1,4 @@
-/* Finding Patients from Hamilton with Allergies Using SQL
+/* Day 2.[28-10-2023]: Finding Patients from Hamilton with Allergies Using SQL
 Easy
 
 Write an SQL query to find a list of patients' first_name, last_name, and allergies who are from the city of Hamilton and have non-null values in the allergies column.

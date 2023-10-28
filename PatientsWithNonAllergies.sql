c6ceaf21060de84bbbf780b8031bd60c21129e46
Thein Naing <theinnaing0908@gmail.com>
2023-10-28 10:11:22 +0800

    Create PatientsWithNonAllergies.sql

---
+++ PatientsWithNonAllergies.sql
@@ -0,0 +1,15 @@
+/* Finding Patients from Hamilton with Allergies Using SQL
+Easy
+
+Write an SQL query to find a list of patients' first_name, last_name, and allergies who are from the city of Hamilton and have non-null values in the allergies column.
+
+Tables Used: patients
+Important Concepts
+SQL Keywords: Understanding SQL keywords like SELECT, FROM, and WHERE will be essential.
+NULL Values: Know how to handle NULL values in SQL, especially when you want to include or exclude them in your results.
+Good luck!   */
+
+SELECT first_name, last_name, allergies
+FROM patients
+WHERE city = 'Hamilton'
+AND allergies IS NOT NULL;
